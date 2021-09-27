35b499631c0e5a2c7102258178691dcd297da7b3
Ahmed Bougacha <ahmed@bougacha.org>
2021-09-27 08:00:00 -0700

    [FIXME] [IR] Generalize EmitGEPOffset to not need an actual GEP inst.

--- llvm/include/llvm/IR/IRBuilder.h
+++ llvm/include/llvm/IR/IRBuilder.h
@@ -2622,6 +2622,7 @@ public:
   IRBuilder(const IRBuilder &) = delete;
 
   InserterTy &getInserter() { return Inserter; }
+  const InserterTy &getInserter() const { return Inserter; }
 };
 
 template <typename FolderTy, typename InserterTy>
