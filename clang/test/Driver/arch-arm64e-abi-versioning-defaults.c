25b75a1aa6ae5634f43cc88d25afd65455aadb0e
Daniil Kovalev <dkovalev@accesssoftek.com>
2023-11-10 20:43:21 +0300

    [clang][AArch64][PAC] Fix arm64e-abi-versioning tests
    Use `-target arm64e-apple-ios` instead of `-arch arm64e`.

--- clang/test/Driver/arch-arm64e-abi-versioning-defaults.c
+++ clang/test/Driver/arch-arm64e-abi-versioning-defaults.c
@@ -1,15 +1,15 @@
 // Check the ABI version support defaults.
 
-// RUN: %clang                                                      -arch arm64e -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
-// RUN: %clang                         -mkernel                     -arch arm64e -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix KERNELABIVERSION
-// RUN: %clang                         -fapple-kext                 -arch arm64e -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix KERNELABIVERSION
+// RUN: %clang                                                      -target arm64e-apple-ios -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
+// RUN: %clang                         -mkernel                     -target arm64e-apple-ios -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix KERNELABIVERSION
+// RUN: %clang                         -fapple-kext                 -target arm64e-apple-ios -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix KERNELABIVERSION
 //
-// RUN: %clang -fno-ptrauth-kernel-abi-version -mkernel                        -arch arm64e -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
-// RUN: %clang -mkernel                        -fno-ptrauth-kernel-abi-version -arch arm64e -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
-// RUN: %clang -fno-ptrauth-kernel-abi-version -fapple-kext                    -arch arm64e -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
-// RUN: %clang -fapple-kext                    -fno-ptrauth-kernel-abi-version -arch arm64e -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
-// RUN: %clang -fno-ptrauth-kernel-abi-version -fptrauth-kernel-abi-version    -arch arm64e -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
-// RUN: %clang -fptrauth-kernel-abi-version    -fno-ptrauth-kernel-abi-version -arch arm64e -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
+// RUN: %clang -fno-ptrauth-kernel-abi-version -mkernel                        -target arm64e-apple-ios -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
+// RUN: %clang -mkernel                        -fno-ptrauth-kernel-abi-version -target arm64e-apple-ios -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
+// RUN: %clang -fno-ptrauth-kernel-abi-version -fapple-kext                    -target arm64e-apple-ios -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
+// RUN: %clang -fapple-kext                    -fno-ptrauth-kernel-abi-version -target arm64e-apple-ios -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
+// RUN: %clang -fno-ptrauth-kernel-abi-version -fptrauth-kernel-abi-version    -target arm64e-apple-ios -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
+// RUN: %clang -fptrauth-kernel-abi-version    -fno-ptrauth-kernel-abi-version -target arm64e-apple-ios -c %s -### 2>&1 | FileCheck %s --check-prefix ABIVERSION-DEFAULT --check-prefix NOKERNELABIVERSION
 //
 // ABIVERSION-DEFAULT: "-fptrauth-abi-version=0"
 // KERNELABIVERSION: "-fptrauth-kernel-abi-version"
