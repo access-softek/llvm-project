0e1dc520c2e5d33d84bcabdd61ef8111ea1f430d
Ilia Kuklin <ikuklin@accesssoftek.com>
2026-06-05 21:42:54 +0500

    Change /Fd description to \'/\'

--- llvm/lib/Target/DirectX/DXContainerGlobals.cpp
+++ llvm/lib/Target/DirectX/DXContainerGlobals.cpp
@@ -42,7 +42,7 @@ using namespace llvm::mcdxbc;
 cl::opt<std::string> PdbDebugPath(
     "dx-Fd",
     cl::desc("Write debug information to the given file, or automatically "
-             "named file in directory when ending in '\\'"),
+             "named file in directory when ending in '/'"),
     cl::value_desc("filename"));
 static cl::opt<bool> ShaderHashDependsOnSource(
     "dx-Zss", cl::desc("Compute Shader Hash considering source information"));
