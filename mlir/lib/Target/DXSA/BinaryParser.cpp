tagolog/access-softek-llvm-project

[mlir][dxsa] Add dcl_indexable_temp instruction
Example:
  dxsa.dcl_indexable_temp x0[23], 2
  dxsa.dcl_indexable_temp x7[4096], 4

Signed-off-by: Vladimir Shiryaev <tagolog@users.noreply.github.com>

mlir/lib/Target/DXSA/BinaryParser.cpp
@@ -516,6 +516,15 @@ public:
                                   builder.getI32IntegerAttr(count));
   }
 
+  Instruction buildDclIndexableTemp(uint32_t registerIndex,
+                                    uint32_t registerCount,
+                                    uint32_t numComponents, Location loc) {
+    return dxsa::DclIndexableTemp::create(
+        builder, loc, builder.getI32IntegerAttr(registerIndex),
+        builder.getI32IntegerAttr(registerCount),
+        builder.getI32IntegerAttr(numComponents));
+  }
+
 private:
   MLIRContext *context;
   ModuleOp module;
@@ -831,6 +840,40 @@ public:
     return builder.buildDclTemps(count, loc);
   }
 
+  FailureOr<Instruction> parseDclIndexableTemp(Location loc) {
+    auto registerIndexToken = parseToken();
+    if (failed(registerIndexToken))
+      return failure();
+    auto registerCountToken = parseToken();
+    if (failed(registerCountToken))
+      return failure();
+    auto numComponentsToken = parseToken();
+    if (failed(numComponentsToken))
+      return failure();
+
+    auto registerCount = *registerCountToken;
+    if (registerCount == 0) {
+      emitError(getLocation(), "indexable temp array size cannot be zero");
+      return failure();
+    }
+    if (registerCount > 4096) {
+      emitError(getLocation(), "invalid indexable temp array size: ")
+          << registerCount << " (max 4096)";
+      return failure();
+    }
+
+    auto numComponents = *numComponentsToken;
+    if (numComponents < 1 || numComponents > 4) {
+      emitError(getLocation(), "indexable temp num components must be in 1..4, "
+                               "got ")
+          << numComponents;
+      return failure();
+    }
+
+    return builder.buildDclIndexableTemp(*registerIndexToken, registerCount,
+                                         numComponents, loc);
+  }
+
   OptionalParseResult parseDclInstruction(uint32_t opcodeToken, Location loc,
                                           Instruction &out) {
     FailureOr<Instruction> result;
@@ -840,6 +883,9 @@ public:
       break;
     case D3D10_SB_OPCODE_DCL_TEMPS:
       result = parseDclTemps(loc);
+      break;
+    case D3D10_SB_OPCODE_DCL_INDEXABLE_TEMP:
+      result = parseDclIndexableTemp(loc);
       break;
     default:
       return std::nullopt;

access-softek/llvm-project

    dxsa.dcl_indexable_temp x<0>[1], 1
    dxsa.dcl_indexable_temp x<1>[23], 2
    dxsa.dcl_indexable_temp x<7>[4096]
Signed-off-by: Vladimir Shiryaev <vshiryaev@accesssoftek.com>

mlir/lib/Target/DXSA/BinaryParser.cpp
@@ -926,6 +926,14 @@ public:
                                         toAttr(ubound), toAttr(space));
   }
 
+  Instruction buildDclIndexableTemp(uint32_t id, uint32_t size,
+                                    uint32_t numComponents, Location loc) {
+    return dxsa::DclIndexableTemp::create(
+        builder, loc, builder.getI32IntegerAttr(id),
+        builder.getI32IntegerAttr(size),
+        builder.getI32IntegerAttr(numComponents));
+  }
+
 private:
   MLIRContext *context;
   OpBuilder builder;
@@ -1951,6 +1959,16 @@ public:
     return builder.buildDclResourceRaw(id, lbound, ubound, space, loc);
   }
 
+  FailureOr<Instruction> parseDclIndexableTemp(Location loc) {
+    auto id = parseToken();
+    FAILURE_IF_FAILED(id);
+    auto size = parseToken();
+    FAILURE_IF_FAILED(size);
+    auto numComponents = parseToken();
+    FAILURE_IF_FAILED(numComponents);
+    return builder.buildDclIndexableTemp(*id, *size, *numComponents, loc);
+  }
+
   OptionalParseResult parseDclInstruction(uint32_t opcodeToken, Location loc,
                                           Instruction &out) {
     FailureOr<Instruction> result;
@@ -2050,6 +2068,9 @@ public:
       break;
     case D3D11_SB_OPCODE_DCL_RESOURCE_RAW:
       result = parseDclResourceRaw(loc);
+      break;
+    case D3D10_SB_OPCODE_DCL_INDEXABLE_TEMP:
+      result = parseDclIndexableTemp(loc);
       break;
     default:
       return std::nullopt;